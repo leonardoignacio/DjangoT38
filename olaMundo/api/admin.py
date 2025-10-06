4195b4620868b3582f4e42500255725b6d78021b
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2025-10-06 11:56:50 -0300

    criação de models Clientes e Produtos e realização de migrations

--- olaMundo/api/admin.py
+++ olaMundo/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Cliente, Produto
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display=('nome', 'sobrenome', 'email')
+    search_fields=('nome', 'sobrenome', 'email')
+    list_filter=('nome', 'sobrenome', 'email')
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display=('nome', 'preco', 'estoque')
+    search_fields=('nome', 'preco', 'estoque')
+    list_filter=('nome',)
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Produto, ProdutoAdmin)
--- olaMundo/api/models.py
+++ olaMundo/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('sobrenome', max_length=100)
+    email = models.CharField('E-mail', max_length=100)
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome} | {self.email}'
+    
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preco', decimal_places=2, max_digits=8)
+    estoque = models.IntegerField('Quantidadode em Estoque')
+    def __str__(self):
+        return f'{self.nome}'     
